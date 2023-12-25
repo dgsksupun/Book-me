62c095fbf4d4a1839a77cdcef9cf8da3132f3e66
dgsksupun <dgsupun.kalana@gmail.com>
2023-12-25 21:52:56 +0530

    log out and add places completed

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -20,7 +20,7 @@ export default function RegisterPage() {
     }
   }
   return (
-    <div className="mt-4 grow flex items-center justify-around">
+    <div className="mt-10 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto" onSubmit={registerUser}>
